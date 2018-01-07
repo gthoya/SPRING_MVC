482d8221575cb2e6c4203c0d71902a6e527435a1
gthoya <gthoya85@hanmail.net>
2018-01-07 16:42:27 +0900

    mybatis 연동

--- src/main/java/com/gthoya/springmvc/test/dao/TestDAO.java
+++ src/main/java/com/gthoya/springmvc/test/dao/TestDAO.java
@@ -1,10 +1,15 @@
 package com.gthoya.springmvc.test.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TestDAO {
+    @Autowired
+    private SqlSession sqlSession;
+
     public String test() {
-        return "Hello World !!!";
+        return sqlSession.selectOne("test.test");
     }
 }
