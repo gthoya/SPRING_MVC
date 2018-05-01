86997c5b60407e87412bd43a77af2a0c6081b167
gthoya <gthoya85@hanmail.net>
2018-05-02 00:08:23 +0900

    ApplicationContext, DataSource 분리

--- src/main/java/com/gthoya/springmvc/configuration/ApplicationContextConfiguration.java
+++ src/main/java/com/gthoya/springmvc/configuration/ApplicationContextConfiguration.java
@@ -19,42 +19,5 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = "com.gthoya.springmvc", excludeFilters = @Filter(Controller.class))
 @Configuration
 public class ApplicationContextConfiguration {
-    @Bean
-    public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
-        dataSource.setUsername("develop");
-        dataSource.setPassword("devteam");
-        dataSource.setInitialSize(1);
-        dataSource.setMinIdle(1);
-        dataSource.setMaxIdle(1);
-        dataSource.setMaxActive(2);
-        dataSource.setValidationQuery("select 1");
-        dataSource.setValidationQueryTimeout(100);
 
-        return dataSource;
-    }
-
-    @Bean
-    public SqlSessionFactoryBean sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-        sqlSessionFactory.setDataSource(dataSource());
-
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        Resource[] resources = resolver.getResources("classpath*:mapper/**/*.xml");
-        sqlSessionFactory.setMapperLocations(resources);
-
-        return sqlSessionFactory;
-    }
-
-    @Bean(destroyMethod = "clearCache")
-    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
-    }
-
-    @Bean
-    public PlatformTransactionManager txManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
 }
