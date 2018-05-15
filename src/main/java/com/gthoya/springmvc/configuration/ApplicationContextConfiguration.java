57758a9ffa4de275814de6f9e089f2c7c564b8b6
jacob <jacob.gt@kakaocorp.com>
2018-05-15 14:57:49 +0900

    불필요 import 제거, signIn page 로그인 사용자 보여주는거 추가

--- src/main/java/com/gthoya/springmvc/configuration/ApplicationContextConfiguration.java
+++ src/main/java/com/gthoya/springmvc/configuration/ApplicationContextConfiguration.java
@@ -1,20 +1,9 @@
 package com.gthoya.springmvc.configuration;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import javax.sql.DataSource;
 
 @ComponentScan(basePackages = "com.gthoya.springmvc", excludeFilters = @Filter(Controller.class))
 @Configuration
--- src/main/webapp/WEB-INF/views/sign/signIn.jsp
+++ src/main/webapp/WEB-INF/views/sign/signIn.jsp
@@ -6,6 +6,7 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>sign in</title>
@@ -24,6 +25,14 @@
 
         function signInCallback(result) {
             alert(result.message);
+
+            if (result.message == "success") {
+                $("#user").text(result.userId + " 님 로그인 하셨습니다.");
+                $("#userDiv").visible(true);
+            } else {
+                $("#user").text("");
+                $("#userDiv").visible(false);
+            }
         }
     </script>
 </head>
@@ -40,6 +49,9 @@
         <div>
             <input type="button" id="signIn" name="signIn" value="로그인">
         </div>
+    </div>
+    <div id="userDiv" style="text-align: center" visible="false">
+        <label id="user"></label>
     </div>
 </body>
 </html>
